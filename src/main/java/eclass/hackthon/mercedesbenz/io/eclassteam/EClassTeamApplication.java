5ddb4753920d6a736874fc0ff1c868a6f6354ab3
Gustavo <ghco@cesar.school>
2023-05-12 18:53:33 +0100

    Initial Commit

---
+++ src/main/java/eclass/hackthon/mercedesbenz/io/eclassteam/EClassTeamApplication.java
@@ -0,0 +1,13 @@
+package eclass.hackthon.mercedesbenz.io.eclassteam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EClassTeamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EClassTeamApplication.class, args);
+    }
+
+}
---
+++ src/test/java/eclass/hackthon/mercedesbenz/io/eclassteam/EClassTeamApplicationTests.java
@@ -0,0 +1,13 @@
+package eclass.hackthon.mercedesbenz.io.eclassteam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EClassTeamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
